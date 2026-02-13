4f208455bf6af0546e22934449f1f58314584faa
Denis <d.kotelev@carprice.tech>
2026-02-13 13:00:14 +0300

    f

--- components/quiz/QuizStepDate.tsx
+++ components/quiz/QuizStepDate.tsx
@@ -63,7 +63,6 @@ export function QuizStepDate({
       <div className="space-y-4 w-full min-w-0">
         <div className="overflow-hidden w-full">
           <input
-            type="date"
             id="birthDate"
             name="birthDate"
             value={value}
